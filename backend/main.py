04ff4bb128a696d677ca17bbcc316652c572bef2
fariz-webdev <fariz.webdeveloper@gmail.com>
2026-08-13 19:53:53 +0700

    Add recommendation engine

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,33 @@
+from services.trip_service import get_trip_category, get_travel_season, calculate_daily_budget, print_recommended_places
+
 #input interaktif
 destination = input("Destination : ")
-country = input("Country : ")
+# country = input("Country : ")
 days = int(input("Days : "))
 budget = float(input("Budget : "))
 currency = input("Currency : ")
 travel_month = input("Travel Month : ")
 
 #function
-def print_trip_summary(destination, country, days, budget, currency, travel_month) :
+def print_trip_summary(destination, days, budget, currency, travel_month) :
+    category = get_trip_category(budget)
+    season = get_travel_season(travel_month)
+    daily = calculate_daily_budget(budget, days)
+
     print()
     print("=========================")
     print("KelanaAI")
     print("=========================")
     print(f"Destination : {destination}")
-    print(f"Country     : {country}")
+    # print(f"Country     : {country}")
     print(f"Days        : {days}")
     print(f"Budget      : {budget} {currency}")
-    print(f"Currency    : {currency}")
+    # print(f"Currency    : {currency}")
+    print(f"Category    : {category}")
+    print(f"Daily Budget: {daily} {currency}/Day")
     print(f"Travel Month: {travel_month}")
+    print(f"Season      : {season}")
+    print_recommended_places()
     print()
 
-print_trip_summary(destination, country, days, budget, currency, travel_month)
+print_trip_summary(destination, days, budget, currency, travel_month)
---
+++ backend/services/trip_service.py
@@ -0,0 +1,26 @@
+recommended_places = ["Tokyo Tower", "Shibuya", "Mount Fuji"]
+
+def get_trip_category(budget):
+  if budget < 1000 :
+    return "Backpacker"
+  elif budget <= 3000 :
+    return "Standart"
+  else :
+    return "Luxury"
+
+def get_travel_season(month):
+  if month.lower() == "december" :
+    return "Peak Season"
+  elif month.lower() == "june" :
+    return "Holiday Season"
+  else :
+    return "Regular Season"
+
+def calculate_daily_budget(budget, days):
+  return budget/days
+
+def print_recommended_places():
+  print()
+  print("Recommended Places")
+  for place in recommended_places:
+    print(f"- {place}")
